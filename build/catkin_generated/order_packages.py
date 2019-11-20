625b7a53bc59040a04d56566a6a9b9e84da7f432
letian <root@letian.net>
2019-11-19 20:27:25 -0800

    commit again

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/letian/test/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/letian/catkin_ws/devel;/home/letian/decision-planning_ncds-sample/devel;/opt/ros/kinetic".split(';') if "/home/letian/catkin_ws/devel;/home/letian/decision-planning_ncds-sample/devel;/opt/ros/kinetic" != "" else []
