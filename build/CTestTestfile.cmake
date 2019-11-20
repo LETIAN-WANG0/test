4b7248e216b89770939c2a8130e13510b7f6de6f
letian <root@letian.net>
2019-11-19 18:40:01 -0800

    Hand in the initialization

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/letian/test/src
+# Build directory: /home/letian/test/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("beginner_tutorials")
---
+++ build/beginner_tutorials/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/letian/test/src/beginner_tutorials
+# Build directory: /home/letian/test/build/beginner_tutorials
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
